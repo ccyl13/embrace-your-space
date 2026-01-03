e6b30f92aad2746147f142ae9cb3cf178a2f8aea
Thomas O´neil Álvarez <168454163+ccyl13@users.noreply.github.com>
2026-01-03 14:16:29 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  // Se ha corregido la ruta base para que coincida exactamente con el nombre del repositorio
+  // Cambia "/volver-a-ti/" por "/Volver-a-ti.YPB/"
   base: mode === "production" ? "/Volver-a-ti.YPB/" : "/",
   server: {
     host: "::",
